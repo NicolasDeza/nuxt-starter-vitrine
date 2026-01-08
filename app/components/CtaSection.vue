0e33c9580823a66fa5da3ae873834ab4c7982ad6
NicolasDeza <nicolasdeza@hotmail.be>
2026-01-08 14:18:32 +0100

    Modif bg mode dark

--- app/components/CtaSection.vue
+++ app/components/CtaSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="w-full bg-[#0F172B] py-10">
-    <div class="mx-auto max-w-[1440px] px-8">
+    <div class="mx-auto max-w-6xl px-6">
       <div class="rounded-lg px-6 py-16 text-center dark:bg-white/5">
         <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
           Prêt à démarrer votre prochain projet Nuxt ?
